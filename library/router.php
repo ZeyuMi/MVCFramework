76be640647f2a50714abfa32d6c4a37a513b2e3c
yzmizeyu <yzmizeyu@gmail.com>
2013-10-18 21:46:54 +0800

    fix bug

--- library/router.php
+++ library/router.php
@@ -1,35 +1,32 @@
 <?php 
 
-function callMethod(){
-	global $default;
-	$request = $_SERVER[PATH_INFO];
-	if(!isset($request)){
-		$controller = $default['controller'];
-		$action = $default['action'];
-	}else{
-		$params = explode('/', substr($request,1));
+setReporting();
 
-		$getVar = array();
-		$controller = array_shift($params);
-		if(isset($params[0])){
-			$action = array_shift($params);
-		}else
-			$action = 'index';
-	}
+$request = $_SERVER[PATH_INFO];
+if(!isset($request)){
+	$controller = $default['controller'];
+	$action = $default['action'];
+}else{
+	$params = explode('/', substr($request,1));
 
-	$controllerName = $controller;
-	$controller = ucfirst($controller);
-	$controller .= 'Controller';
-
-	$controllerObject = new $controller($controllerName, $action);
-	
-	$template = new Template($controllerName, $action);
-	if((int)method_exists($controller, $action)){
-		call_user_func(array($controllerObject, $action));
-	}
-	$template->render();
+	$getVar = array();
+	$controller = array_shift($params);
+	if(isset($params[0])){
+		$action = array_shift($params);
+	}else
+		$action = 'index';
 }
 
-setReporting();
-callMethod();
+$controllerName = $controller;
+$controller = ucfirst($controller);
+$controller .= 'Controller';
+
+$controllerObject = new $controller($controllerName, $action);
+
+$template = new Template($controllerName, $action);
+if((int)method_exists($controller, $action)){
+	call_user_func(array($controllerObject, $action));
+}
+$template->render();
+
 
